7b977a623619fe33884c4769675d711321c84f5d
NaimBiswas <nayeembiswas2@gmail.com>
2023-12-24 16:24:26 +0600

    REQ.IP

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.json({ipPac:ip.address(), remoteIP: req.socket.localAddress })
+    res.json({ipPac:ip.address(), remoteIP: req.socket.localAddress, reqIp: req.ip, })
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
