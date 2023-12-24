69b0c380989faffc0ac8a7ec1f074888e44ffaa4
NaimBiswas <nayeembiswas2@gmail.com>
2023-12-24 16:30:55 +0600

    Added Os Network inferfaces

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const ip = require('ip')
 const app = express()
 const port = 3000
+var os = require('os');
 
 app.get('/', (req, res) => {
-    res.json({ipPac:ip.address(), remoteIP: req.socket.localAddress, reqIp: req.ip, })
+    var networkInterfaces = os.networkInterfaces();
+    console.log(networkInterfaces);
+
+    res.json({ipPac:ip.address(), remoteIP: req.socket.localAddress, reqIp: req.ip,networkInterfaces:networkInterfaces })
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
