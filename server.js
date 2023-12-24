e5e9468423bde179617e954a953568a4eb719b47
NaimBiswas <nayeembiswas2@gmail.com>
2023-12-24 16:34:04 +0600

    Added Os Network inferfaces IPV4

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const port = 3000
 var os = require('os');
 
 app.get('/', (req, res) => {
-    var networkInterfaces = os.networkInterfaces();
-    console.log(networkInterfaces);
+    const networkInterfaces = os.networkInterfaces();
+
+    // Find the IP address associated with the network interfaces
+    const systemIP = Object.keys(networkInterfaces)
+      .map(interfaceName =>
+        networkInterfaces[interfaceName].find(
+          iface => iface.family === 'IPv4' && !iface.internal
+        )
+      )
+      .filter(Boolean)[0].address;
+    
+    console.log('System IP address:', systemIP);
 
     res.json({ipPac:ip.address(), remoteIP: req.socket.localAddress, reqIp: req.ip,networkInterfaces:networkInterfaces })
 })
