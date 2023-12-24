7fdecca3dc761376551e18eb1add9b903100a0b0
NaimBiswas <nayeembiswas2@gmail.com>
2023-12-24 16:13:34 +0600

    Ip Server in NodeJS to get the Client requested IP

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const ip = require('ip')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.json({ipPac:ip.address(), remoteIP: req.socket.localAddress })
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
