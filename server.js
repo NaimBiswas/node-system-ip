690d5f54a7726b2bd794506ae86a6a8b162774c5
NaimBiswas <nayeembiswas2@gmail.com>
2024-02-01 14:28:53 +0600

    fix port issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const ip = require('ip')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 var os = require('os');
 const axios = require('axios');
 app.set('trust proxy', true);
